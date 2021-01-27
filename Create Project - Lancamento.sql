f4640d96b5677679430d6dbef8feae3c6a658100
Gerson Junior <gersonkenedy@hotmail.com>
2021-01-27 13:41:35 -0300

    adicinando retorno erro e exception

--- Create Project - Lancamento.sql
+++ Create Project - Lancamento.sql
@@ -5,19 +5,30 @@
 -- Data   : 27/01/2021
 -- Descr. : Criando Projeto
 
+-- ---------------------- -------------------------- ----------------------
+
+-- Versao : M002
+-- Autor  : Gerson Batista
+-- Data   : 27/01/2021
+-- Descr. : Adicionando validações e retorno
+
 -- ---------------------- Package ------------------------------------------
 
-CREATE OR REPLACE PACKAGE PKG_LANCAMENTOS AS 
+CREATE OR REPLACE PACKAGE PKG_LANCAMENTOS AS
        
-    PROCEDURE CRIAR(V_ROW IN LANCAMENTO%ROWTYPE); --M001
+    PROCEDURE CRIAR(V_ROW IN LANCAMENTO%ROWTYPE, --M001
+        V_ERRO OUT VARCHAR2); --M002
 
-END PKG_LANCAMENTOS; --M001
+END PKG_LANCAMENTOS;
+
 
 -- ---------------------- Package Body ------------------------------------------
 
 CREATE OR REPLACE PACKAGE BODY PKG_LANCAMENTOS AS
 
-      PROCEDURE CRIAR(V_ROW IN LANCAMENTO%ROWTYPE) is
+     PROCEDURE CRIAR(V_ROW IN LANCAMENTO%ROWTYPE, 
+        V_ERRO OUT VARCHAR2) is --M002
+
 
       BEGIN
 
@@ -40,8 +51,14 @@ CREATE OR REPLACE PACKAGE BODY PKG_LANCAMENTOS AS
                                         V_ROW.codigo_categoria,
                                         V_ROW.codigo_pessoa); --M001
 
-        COMMIT; --M001
-        DBMS_OUTPUT.PUT_LINE('Dados inseridos com sucesso.'); --M001
+       DBMS_OUTPUT.PUT_LINE('Dados inseridos com sucesso.');
+        COMMIT;
+        
+        EXCEPTION --M002
+          WHEN OTHERS THEN --M002
+            ROLLBACK; --M002
+               V_ERRO := 'Erro ao inserir dados: ' || SQLERRM; --M002
+            --DBMS_OUTPUT.PUT_LINE('Erro ao inserir dados: ' || SQLERRM); 
 
       END; --M001
 
@@ -51,11 +68,12 @@ END PKG_LANCAMENTOS; --M001
 
 declare 
   V_ROW lancamento%ROWTYPE; --M001
+  vData lancamento.data_vencimento%TYPE := sysdate; --M002
 begin
   
   V_ROW.codigo := seq_lancamento.nextval; --M001
   V_ROW.descricao := 'Teste'; --M001
-  V_ROW.data_vencimento := sysdate; --M001
+  V_ROW.data_vencimento := vData; --M002
   V_ROW.data_pagamento := sysdate; --M001
   V_ROW.valor := 100.00; --M001
   V_ROW.observacao := 'OBS teste'; --M001
@@ -63,7 +81,8 @@ begin
   V_ROW.codigo_categoria := 1; --M001
   V_ROW.codigo_pessoa := 1; --M001
  
-  pkg_lancamentos.criar(V_ROW => V_ROW); --M001
+  pkg_lancamentos.criar(v_row => v_row,  --M001
+                        v_erro => :v_erro);  --M002
 end; --M001
 
 -- ---------------------------------------------------------------------------
