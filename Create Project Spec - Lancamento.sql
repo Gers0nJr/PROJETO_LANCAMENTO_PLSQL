26733c5b5715bab7861e6241e6d2fbf9da9b58bf
Gerson Batista <32967308+Gers0nJr@users.noreply.github.com>
2021-02-01 14:33:47 +0300

    Create Create Project Spec - Lancamento.sql

---
+++ Create Project Spec - Lancamento.sql
@@ -0,0 +1,70 @@
+-- ---------------------- Historico de Manutencao ------------------------------------------
+
+-- Versao : M001
+-- Autor  : Gerson Batista
+-- Data   : 27/01/2021
+-- Descr. : Criando Projeto
+
+-- ---------------------- -------------------------- ----------------------
+
+-- Versao : M002
+-- Autor  : Gerson Batista
+-- Data   : 27/01/2021
+-- Descr. : Adicionando validações e retorno
+
+-- ---------------------- -------------------------- ----------------------
+
+-- Versao : M003
+-- Autor  : Gerson Batista
+-- Data   : 28/01/2021
+-- Descr. : Adicionando codigo no retorno proc
+
+-- ---------------------- -------------------------- ----------------------
+
+-- Versao : M004
+-- Autor  : Gerson Batista
+-- Data   : 27/01/2021
+-- Descr. : Adicionando deletar e validação codigo
+
+-- ---------------------- Package V4------------------------------------------
+
+CREATE OR REPLACE PACKAGE PKG_LANCAMENTOS AS
+
+    msg_geral EXCEPTION;
+    msg_erro EXCEPTION;
+
+    TYPE rows IS TABLE OF lancamento%ROWTYPE 
+    INDEX BY PLS_INTEGER;
+    
+    t_lancamento rows;
+    
+    /*FUNCTION VALIDAR_CODIGO(
+      P_CODIGO IN LANCAMENTO.CODIGO%TYPE) 
+      return LANCAMENTO.DESCRICAO%TYPE;*/
+      
+    /*FUNCTION VALIDAR_CODIGO(
+      P_CODIGO IN LANCAMENTO.CODIGO%TYPE, 
+      V_COD_RET OUT NUMBER,
+      V_RETORNO OUT VARCHAR2
+      ) 
+      return LANCAMENTO.CODIGO%TYPE;*/
+      
+      FUNCTION VALIDAR_CODIGO(
+      P_CODIGO IN LANCAMENTO.CODIGO%TYPE
+      ) 
+      return BOOLEAN;
+       
+    PROCEDURE CRIAR(V_ROW IN LANCAMENTO%ROWTYPE, --M001
+        V_COD_RET OUT NUMBER,  --M003
+        V_RETORNO OUT VARCHAR2); --M003
+  
+    PROCEDURE EXCLUIR(P_CODIGO IN LANCAMENTO.CODIGO%TYPE,
+        V_COD_RET OUT NUMBER,
+        V_RETORNO OUT VARCHAR2);
+        
+     PROCEDURE ATUALIZAR(P_CODIGO IN LANCAMENTO.CODIGO%TYPE,
+        V_ROW IN LANCAMENTO%ROWTYPE,
+        V_COD_RET OUT NUMBER,
+        V_RETORNO OUT VARCHAR2);
+    
+END PKG_LANCAMENTOS;
